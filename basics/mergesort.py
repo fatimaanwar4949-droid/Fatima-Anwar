31f0b79cefb8af768f25ad5916574bc421097c83
Fatima <yourname.surname@tuni.fi>
2026-01-03 20:30:19 +0200

    Restore mergesort.py to course version for submission

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,8 +1,19 @@
+
+def debug_print(debug_msg=None, **kwargs):
+
+    if debug_msg:
+        print(debug_msg)
+
+    for key, value in kwargs.items():
+        print("{}: {}".format(key, value))
+
+
 def mergesort(array):
     if len(array) <= 1:
         return array
 
     m = len(array) // 2
+
     left = mergesort(array[:m])
     right = mergesort(array[m:])
 
@@ -18,8 +29,10 @@ def merge(left, right):
         else:
             merged.append(right.pop(0))
 
-    merged += left
-    merged += right
+    if len(left) > 0:
+        merged += left
+    else:
+        merged += right
 
     return merged
 
@@ -27,7 +40,13 @@ def merge(left, right):
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
     input_list = input_str.split(",")
-    value_list = [int(x) for x in input_list]
+    value_list = []
+    for x in input_list:
+        try:
+            value_list.append(int(x))
+        except ValueError as err:
+            print("Invalid input.")
+            quit(1)
 
     sorted_list = mergesort(value_list)
     print(sorted_list)
